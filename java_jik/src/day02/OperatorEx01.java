1e166baa0b97a9d79aba1b9d026d74030eb51d9b
seyoung <kimsyty@naver.com>
2022-10-25 21:41:29 +0900

    자바 02일차: NOT 연산자 예제

---
+++ java_jik/src/day02/OperatorEx01.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class OperatorEx01 {
+
+	public static void main(String[] args) {
+		//!(Not) 연산자 : 참 또는 거짓인 값(변수, 리터럴, 조건식) 앞에 옴
+		boolean isMale = true;
+		boolean isFemale = false;
+		System.out.println("남성입니까? : "+isMale+","+!isFemale);
+		System.out.println("여성입니까? : "+!isMale+","+isFemale); //논리부정연산자 !
+		
+		int num = 1;
+//		System.out.println(!num); // 정수 앞에 안됨
+	}
+
+}
