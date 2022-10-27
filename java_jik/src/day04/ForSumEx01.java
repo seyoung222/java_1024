e87d5f3c2130dac77b1a1da31259f1695eb65b9f
seyoung <kimsyty@naver.com>
2022-10-27 17:12:10 +0900

    자바 04일차: for문 - 1부터 10사이 짝수의 합 예제

---
+++ java_jik/src/day04/ForSumEx01.java
@@ -0,0 +1,22 @@
+package day04;
+
+public class ForSumEx01 {
+
+	public static void main(String[] args) {
+		/* 1부터 10 사이의 짝수의 합을 구하는 코드를 작성하세요. 
+		 * 반복횟수 : i는 1부터 10까지 1씩 증가
+		 * 규칙성 : i가 짝수이면 sum += i를 실행
+		 * 반복문 종료 후 : sum을 출력
+		 * */
+		
+		int sum = 0;
+		for(int i=1; i<=10; i++) {
+			if(i%2==0) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+		
+		
+	}
+}
