e49a5f393de1f105ac54079ff983681299324a92
seyoung <kimsyty@naver.com>
2022-11-06 16:15:45 +0900

    베라31 완

--- java_jik/src/day10/Homework31.java
+++ java_jik/src/day10/Homework31.java
@@ -26,55 +26,95 @@ public class Homework31 {
 		do {
 			printMenu();
 			menu=inputInt();
-			runMenu(menu, comWin, userWin);
+			int winner = runMenu(menu, comWin, userWin);
+			//게임 후 리턴된 값이 1이면 컴 1승추가, 2면 사용자 1승추가
+			if(winner==1) {
+				comWin++;
+			} else {
+				userWin++;
+			}
+			
 		}while(menu!=3);
 	}
 	
 	
 	
-	public static void runMenu(int menu, int comWin,int userWin) {
+	public static int runMenu(int menu, int comWin,int userWin) {
 		switch(menu) {
 		case 1:
-			
-			int [] game;
-			int lastNum=0;
-			
-			do{
-				//31칸짜리 배열을 만들고
-				game = new int[31];
-				//컴퓨터가 1~3 랜덤숫자를 입력, lastNum리턴
-				//31넘는지 확인 => 사용자 1승 추가
-				if(lastNum>=31) {
-					userWin++;
-					break;
-				}
-				//사용자가 1~3 랜덤숫자를 입력, lastNum리턴
-				//31넘는지 확인 => 컴퓨터 1승 추가
-				if(lastNum>=31) {
-					comWin++;
-					break;
-				}
-			}while(lastNum<31);
-			//컴퓨터
-			break;
+			//게임을 플레이하고 컴퓨터가 이기면 1, 사용자가 이기면 2를 리턴
+			int winner = newGame();
+			if(winner==1) {
+				return 1;
+			} else {
+				return 2;
+			}
 		case 2:
 			printScore(comWin, userWin);
-			break;
+			return 0;
 		case 3:
 			System.out.println("프로그램을 종료합니다.");
-			break;
+			return 0;
 		default:
 			System.out.println("잘못된 메뉴입니다.");
+			return 0;
 		}
 	}
 	
-	//주어진 배열에서 비어있는 칸부터 1~3랜덤수를 이어서 채워넣고, 채워넣은 숫자들을 출력하는 메소드
-	public static int callNum(int[]game) {
-		int lastNum;
-		
-		return 0;
-	}
 	
+	private static int newGame() {
+		int [] game=new int[31];
+		int lastIndex=-1;
+		boolean doesComWin=false;
+		
+		do{
+			//컴퓨터가 1~3 랜덤숫자를 입력, lastIndex갱신
+			int callNum = random(1,3);
+			System.out.print("[컴퓨터] ");
+			for(int i=lastIndex+1; i<=lastIndex+callNum; i++) {
+				if(i<31) {
+					game[i]=i+1;
+					System.out.print(game[i]+" ");
+				} else if(i>=31) {
+					break;
+				}
+			}
+			System.out.println();
+			lastIndex += callNum;
+			//31넘는지 확인 => 사용자 1승 추가
+			if(lastIndex>=30) {
+				System.out.println("사용자가 승리하였습니다.");
+				doesComWin=false;
+				break;
+			}
+			
+			//사용자가 1~3 랜덤숫자를 입력, lastIndex갱신
+			Scanner scan = new Scanner(System.in);
+			System.out.print("입력> ");
+			callNum = scan.nextInt();
+			System.out.print("[사용자] ");
+			for(int i=lastIndex+1; i<=lastIndex+callNum; i++) {
+				if(i<31) {
+					game[i]=i+1;
+					System.out.print(game[i]+" ");
+				} else if(i>=31) {
+					break;
+				}
+			}
+			System.out.println();
+			lastIndex += callNum;
+			//31넘는지 확인 => 컴퓨터 1승 추가
+			if(lastIndex>=30) {
+				System.out.println("컴퓨터가 승리하였습니다.");
+				doesComWin=true;
+				break;
+			}
+		}while(lastIndex<30);
+		System.out.println("게임을 종료합니다.");
+		
+		return doesComWin ? 1 : 2;
+	}
+
 	public static void printScore(int comWin, int userWin) {
 		System.out.println("*** 현재까지 스코어 ***");
 		System.out.println("컴퓨터: "+comWin+"승");
