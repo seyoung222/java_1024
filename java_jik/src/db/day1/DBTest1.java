1b14f95200184ec7130e4ad872852a36cf2d1e3d
seyoung <kimsyty@naver.com>
2022-12-26 23:37:23 +0900

    DB 기본 설명

--- java_jik/src/db/day1/DBTest1.java
+++ java_jik/src/db/day1/DBTest1.java
@@ -10,31 +10,27 @@ public class DBTest1 {
  * (C드라이브\Program Files(X86)\MySQL\Connector J 8.0 선택 후 Apply
  * */
 	public static void main(String[] args) {
-		// Connection 객체를 자동완성으로 import할 때는 com.mysql.connection이 아닌
-        // java 표준인 java.sql.Connection 클래스를 import해야 한다.
 		Connection conn = null;
+		//java와 DB를 연결하기 위한 Connection클래스의 객체를 생성(선언은 예외처리해야해서 초기화만 한 상태)
+		//꼭 다시 close해야하고 DB와 데이터를 주고 받는 동안에는 꼭 연결되어있어야함(=close되지 않은 상태)
 
         try{
-            // 1. 드라이버 로딩
-            // 드라이버 인터페이스를 구현한 클래스를 로딩
-            // mysql, oracle 등 각 벤더사 마다 클래스 이름이 다르다.
-            // mysql은 "com.mysql.jdbc.Driver"이며, 이는 외우는 것이 아니라 구글링하면 된다.
-            // 참고로 이전에 연동했던 jar 파일을 보면 com.mysql.jdbc 패키지에 Driver 라는 클래스가 있다.
-            Class.forName("com.mysql.cj.jdbc.Driver");//이 클래스가 있는지 찾아주고 연결도 해줌
-            /* mysql : DBMS
-             * localhost : 내부 로컬 주소-> 프로그램이 실행되는 서버와 DB가 같은 PC에 있는 경우
-             * university : 데이터베이스 명
-             * */
+            Class.forName("com.mysql.cj.jdbc.Driver");//드라이버 로딩: 이 클래스가 있는지 찾아주고 연결도 해줌
+            //드라이버 인터페이스를 구현한 클래스를 로딩하는건데 버전이 높아지면서 이젠 이걸 굳이 안써도 실행O
+            //-> 단, 이 명령어로 인해 ClassNotFoundException이 생긴 것이므로 이거 지우면 해당 catch도 삭제
             
-            // 2. 연결하기
-            // 드라이버 매니저에게 Connection 객체를 달라고 요청한다.
-            // Connection을 얻기 위해 필요한 url 역시, 벤더사마다 다르다.
-            // mysql은 "jdbc:mysql://localhost/사용할db이름" 이다.
-            String url = "jdbc:mysql://localhost/university";
             
+            String url = "jdbc:mysql://localhost/university"; // "jdbc:DBMS명://서버IP/DB명"
             String id = "root";
             String pw = "root";
             conn = DriverManager.getConnection(url, id, pw);
+            /* url
+             * mysql : DBMS
+             * localhost : 내부 로컬 주소-> 프로그램이 실행되는 서버와 DB가 같은 PC에 있는 경우
+             * 		서버와 DB프로그램이 다른 PC에 있는 경우 다른 PC의 아이피 주소를 적을 것.
+             * university : 데이터베이스 명
+             * */
+            
             System.out.println("연결 성공");
 
         }
@@ -45,6 +41,7 @@ public class DBTest1 {
             System.out.println("에러: " + e);
         }
         finally{
+        	// -> 수업에서는 finally이하내용을 closeConnect() 메소드로 생성함.
             try{
                 if( conn != null && !conn.isClosed()){
                     conn.close();
--- java_jik/src/db/day1/DBTest1_1.java
+++ java_jik/src/db/day1/DBTest1_1.java
@@ -11,10 +11,11 @@ import java.util.ArrayList;
 import lombok.Data;
 
 public class DBTest1_1 {
-// DBTest1의 내용을 사용해 메소드를 이용해서 DB연결
+// DBTest1의 내용을 사용해 메소드를 이용해서 DB연결하는 코드
+	static ResultSet rs = null; //워크벤치에서 select했을때 출력되는 결과표를 ResultSet이라고 함!
 	static Statement stmt = null;
-	static ResultSet rs = null;
 	static PreparedStatement pstmt = null;
+	//Statement와 PreparedStatement는 sql문 수행에 사용하는 클래스인데 차이점은 아래 메소드에 설명 적혀있음
 	
 	public static void main(String[] args) {
 		Connection con = null;
@@ -23,53 +24,27 @@ public class DBTest1_1 {
         String id = "root";
         String pw = "root";
         ArrayList<Student1> list = null;
-        con = connect(url, id, pw);
+        con = connect(url, id, pw); //DB연결하고 커넥션 객체를 리턴함
         
         try {
 			stmt = con.createStatement(); //메소드의 오류처리 try-catch문 사용
 			//insertStudent(con, "2022160004", "라길동",1,"재학","2022160002");
 			//updateStudent(con, "2022160001", "가나다");
-			deleteStudent(con, "2022160005");
+			//deleteStudent(con, "2022160005");
 			
+			//학생정보 조회
 			list = selectAllStudent(con);
 			System.out.println(list);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-      
         connectClose(con);
 	}
-	private static void deleteStudent(Connection con, String st_num) throws SQLException {
-		String sql = "delete from student where st_num = ?";
-		pstmt = con.prepareStatement(sql);
-		pstmt.setString(1, st_num);
-		int count = pstmt.executeUpdate();
-		if(count == 0) {
-			System.out.println("[삭제 실패]");
-		}else {
-			System.out.println("[삭제 성공]");
-		}
-		
-	}
-	private static void updateStudent(Connection con,String st_num,String st_name) throws SQLException {
-		String sql = "update student set st_name =? where st_num = ?";
-		pstmt = con.prepareStatement(sql);
-		pstmt.setString(1, st_name);
-		pstmt.setString(2, st_num);
-		int count = pstmt.executeUpdate();
-		if(count == 0) {
-			System.out.println("[수정 실패]");
-		}else {
-			System.out.println("[수정 성공]");
-		}
-		
-	}
-	//앞에서 한 작업을 메소드로 만들기
+	//DB연결하고 커넥션 객체를 리턴하는 메소드
 	public static Connection connect(String url,String id,String pw) {
 		Connection con = null;
 		try{
-            Class.forName("com.mysql.cj.jdbc.Driver");//이 클래스가 있는지 찾아주고 연결도 해줌
-            
+            Class.forName("com.mysql.cj.jdbc.Driver");//이 클래스가 있는지 찾아주고 연결도 해줌            
             con = DriverManager.getConnection(url, id, pw);
             System.out.println("[연결 성공]");
 	        }
@@ -92,20 +67,21 @@ public class DBTest1_1 {
             e.printStackTrace();
         }
 	}
-	
+	//저장된 Student 테이블을 list에 저장해서 list를 반환하는 메소드
+	//select * from Student해서 resultSet에 나온 결과(=모든 데이터)의 한줄 한줄을 list에 하나씩 넣는 작업
 	public static ArrayList<Student1> selectAllStudent(Connection con) throws SQLException{
 		String sql = "select st_num, st_name, st_semester, st_state, st_pr_num from student"; //쿼리를 저장
 		rs = stmt.executeQuery(sql); //쿼리 실행하고 출력표를 반환
 		ArrayList<Student1> list = new ArrayList<Student1>();
-		while(rs.next()) { //표에 있는 걸 한줄씩 불러와야함
-			String st_num = rs.getString(1);
-			String st_name = rs.getString(2);
-			int st_semester = rs.getInt(3);
+		while(rs.next()) { //표에 있는 걸 한줄씩 불러와야함. 내용이 없을 때까지 반복실행.
+			String st_num = rs.getString(1); //1번째 행(속성)의 값을 st_num에 저장
+			String st_name = rs.getString(2); //2번째 행에 있는 값을 st_name에 저장 
+			int st_semester = rs.getInt(3); //이하 동문
 			String st_state = rs.getString(4);
 			String st_pr_num = rs.getString(5);
 			Student1 std = new Student1(st_num, st_name, st_semester, st_state, st_pr_num);
-			list.add(std);
-			//System.out.println(st_num+","+st_name+","+st_semester+","+st_state+","+st_pr_num);
+			//위에서 저장한 값들을 이용해 속성과 동일한 필드를 가진 Student1 클래스의 객체를 생성하고
+			list.add(std);//리스트에 저장
 		}
 		return list;
 		/* select문에서도 조건을 매번 바꿔서 사용하고 싶을때 ?와 prepareState 사용가능
@@ -121,17 +97,43 @@ public class DBTest1_1 {
 			String st_state, String st_pr_num) throws SQLException {
 		String sql = "insert into student(st_num, st_name,st_semester, st_state, st_pr_num)"+
 				"values(?, ?, ?, ?, ?)";
+		//위 sql문처럼 고정되지 않은 값이 있으면 stmt가 아니라 pstmt를 사용.
+		//즉 select * from ~ 처럼 내용이 고정된 sql은 stmt를, 사용자에게 입력받아 변화하는 값을 넣어야하는 경우 pstmt
 		pstmt = con.prepareStatement(sql);
-		pstmt.setString(1, st_num);
-		pstmt.setString(2, st_name);
+		pstmt.setString(1, st_num); //매개변수로 받은 st_num문자열을 1번째 ?에 집어넣음
+		pstmt.setString(2, st_name); 
 		pstmt.setInt(3, st_semester);
 		pstmt.setString(4, st_state);
 		pstmt.setString(5, st_pr_num);
-		int count = pstmt.executeUpdate(); // 쿼리실행후 실행된 명령어 수 리턴
-		if(count == 0) {
+		int count = pstmt.executeUpdate(); // ?에 값을 모두 대입하고 완성된 sql문을 실행
+		// 쿼리 실행 후 무사히 실행된 명령어의 개수를 count에 리턴
+		if(count == 0) { //제대로 실행된 명령어가 하나도 없다 = 제대로 작동하지 않았다.
 			System.out.println("[추가 실패]");
 		}else {
 			System.out.println("[추가 성공]");
+		}
+	}
+	private static void updateStudent(Connection con,String st_num,String st_name) throws SQLException {
+		String sql = "update student set st_name =? where st_num = ?";
+		pstmt = con.prepareStatement(sql);
+		pstmt.setString(1, st_name);
+		pstmt.setString(2, st_num);
+		int count = pstmt.executeUpdate();
+		if(count == 0) {
+			System.out.println("[수정 실패]");
+		}else {
+			System.out.println("[수정 성공]");
+		}
+	}
+	private static void deleteStudent(Connection con, String st_num) throws SQLException {
+		String sql = "delete from student where st_num = ?";
+		pstmt = con.prepareStatement(sql);
+		pstmt.setString(1, st_num);
+		int count = pstmt.executeUpdate();
+		if(count == 0) {
+			System.out.println("[삭제 실패]");
+		}else {
+			System.out.println("[삭제 성공]");
 		}
 	}
 }
