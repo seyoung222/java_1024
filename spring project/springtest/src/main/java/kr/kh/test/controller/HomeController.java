1dbe62fdb5e051c9b2af5fc115c74a455a264e63
seyoung <kimsyty@naver.com>
2023-02-10 19:13:09 +0900

    스프링 복습 - DB 연동 완료

--- spring project/springtest/src/main/java/kr/kh/test/controller/HomeController.java
+++ spring project/springtest/src/main/java/kr/kh/test/controller/HomeController.java
@@ -1,15 +1,22 @@
 package kr.kh.test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import kr.kh.test.service.MemberService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	
+	@Autowired
+	MemberService memberService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView home(ModelAndView mv) {
 		mv.setViewName("/main/home");
